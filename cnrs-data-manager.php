c63a82d81e12746b77de8b37cc16817ad2000f36
fred <in-touch-fg@quanticalsolutions.com>
2024-01-26 09:32:09 +0100

    first commit

---
+++ cnrs-data-manager.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * CNRS Data Manager
+ *
+ * Plugin Name: CNRS Data Manager
+ * Plugin URI:  https://wordpress.org/plugins/classic-editor/
+ * Description: Allows Wordpress to be powered by external data provided by an XML file on the agents, services, platforms and teams of the UMR EPOC.
+ * Version:     1.0.0
+ * Author:      QS Conseils
+ * Author URI:  https://github.com/WordPress/classic-editor/
+ * License:     GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: cnrs-data-manager
+ * Domain Path: /languages
+ * Requires at least: 4.9
+ * Requires PHP: 8.1.0
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 2, as published by the Free Software Foundation. You may NOT assume
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
---
+++ readme.txt
@@ -0,0 +1,9 @@
+=== CNRS Data Manager ===
+Contributors: CNRS, Epoc, QS Conseils
+Tags: data manager
+Requires at least: 4.9
+Tested up to: 6.2
+Stable tag: 1.6.3
+Requires PHP: 8.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
